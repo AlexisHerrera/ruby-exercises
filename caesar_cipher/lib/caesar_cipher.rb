aea22ddc96e8173fefba327c2fa2df16d002d90f
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-29 23:39:41 -0300

    Test shift in 1

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,5 +1,10 @@
-# frozen_string_literal: true
-
-def caesar_cipher(text, _key)
-  text
+def caesar_cipher(text, key)
+  return text if key.zero?
+  alphabet = ("a".."z").to_a
+  result = ""
+  text.split('').each do |letter|
+    index = alphabet.index(letter)
+    result << alphabet[index + 1]
+  end
+  result
 end
--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -1,13 +1,17 @@
-# frozen_string_literal: true
-
 require 'spec_helper'
 require_relative '../lib/caesar_cipher'
 
 RSpec.describe 'Caesar Cipher' do
   describe 'Caesar Cipher without shifting' do
-    it 'Cipher with 0 should return the same text' do
+    it 'Cipher with key 0 should return the same text' do
       text_to_cipher = 'hello'
       expect(caesar_cipher(text_to_cipher, 0)).to eq 'hello'
+    end
+  end
+  describe 'Caesar Cipher shifting 1' do
+    it 'Cipher with key 1 should shift in 1 position' do
+      text_to_cipher = 'hello'
+      expect(caesar_cipher(text_to_cipher, 1)).to eq 'ifmmp'
     end
   end
 end
