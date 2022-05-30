6d30c0df84adaa16887ef794a580a79b18b80bcb
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-30 00:27:17 -0300

    Remove (false) comment

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -2,7 +2,6 @@ def caesar_cipher(text, key)
   text.chars.map { |letter| cipher_letter(letter, key) }.join
 end
 
-# Only ciphers alphanumeric
 def cipher_letter(char, key)
   return char if char.upcase == char.downcase # is not a char
 
