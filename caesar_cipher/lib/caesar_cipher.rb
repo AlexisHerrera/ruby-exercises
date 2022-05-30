b18f3a89a12eede535f5664e248e9924734b946a
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-30 00:03:15 -0300

    Rubocop lint

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,4 +1,4 @@
 def caesar_cipher(text, key)
   alphabet = ('a'..'z').to_a
-  text.chars.map { |letter| alphabet[(alphabet.index(letter) + key)%26]}.join("")
+  text.chars.map { |letter| alphabet[(alphabet.index(letter) + key) % alphabet.length] }.join('')
 end
