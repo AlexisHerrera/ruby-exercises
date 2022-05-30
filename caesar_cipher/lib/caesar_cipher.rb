510c9e18cef46a521d8e601ef44b46876ad60f35
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-29 23:57:25 -0300

    Refactor to use map

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,9 +1,4 @@
 def caesar_cipher(text, key)
   alphabet = ('a'..'z').to_a
-  result = ''
-  text.split('').each do |letter|
-    index = alphabet.index(letter)
-    result << alphabet[(index + key)%26]
-  end
-  result
+  text.chars.map { |letter| alphabet[(alphabet.index(letter) + key)%26]}.join("")
 end
