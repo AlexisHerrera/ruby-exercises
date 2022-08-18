3447bec83159ae60a312838e813d6cedeb4b169c
Alexis Herrera <alexysaguilar@gmail.com>
2022-08-18 18:04:12 -0300

    Game logic completed

--- tic-tac-toe/lib/board_analyzer.rb
+++ tic-tac-toe/lib/board_analyzer.rb
@@ -8,9 +8,83 @@ class BoardAnalyzer
   end
 
   def game_finished?
-    # There are 8 posible ways. rows(+3), columns (+3) and diagonals (+2)
-    return true if @board.cross_cell_at?(0, 0) && @board.cross_cell_at?(0, 1) && @board.cross_cell_at?(0, 2)
+    return true if player_one_won? || player_two_won? || draw?
 
     false
+  end
+
+  def player_one_won?
+    return true if any_row_only_cross? || any_column_only_cross? || any_diagonal_only_cross?
+
+    false
+  end
+
+  def player_two_won?
+    return true if any_row_only_circle? || any_column_only_circle? || any_diagonal_only_circle?
+
+    false
+  end
+
+  def draw?
+    (0..2).each do |row_index|
+      (0..2).each do |column_index|
+        return false if @board.empty_at?(row_index, column_index)
+      end
+    end
+    true
+  end
+
+  private
+
+  def any_row_only?(match_method)
+    (0..2).each do |row_index|
+      row_has_only_element_type = match_method.call(row_index, 0) &&
+                                  match_method.call(row_index, 1) &&
+                                  match_method.call(row_index, 2)
+      return true if row_has_only_element_type
+    end
+    false
+  end
+
+  def any_column_only?(match_method)
+    (0..2).each do |column_index|
+      row_has_only_element_type = match_method.call(0, column_index) &&
+                                  match_method.call(1, column_index) &&
+                                  match_method.call(2, column_index)
+      return true if row_has_only_element_type
+    end
+    false
+  end
+
+  def any_diagonal_only?(match_method)
+    return true if match_method.call(0, 0) && match_method.call(1, 1) && match_method.call(2, 2)
+    return true if match_method.call(2, 0) && match_method.call(1, 1) && match_method.call(0, 2)
+
+    false
+  end
+
+  def any_row_only_cross?
+    any_row_only?(@board.method(:cross_cell_at?))
+  end
+
+  def any_column_only_cross?
+    any_column_only?(@board.method(:cross_cell_at?))
+  end
+
+  def any_diagonal_only_cross?
+    any_diagonal_only?(@board.method(:cross_cell_at?))
+  end
+
+  # Circles
+  def any_row_only_circle?
+    any_row_only?(@board.method(:circle_cell_at?))
+  end
+
+  def any_column_only_circle?
+    any_column_only?(@board.method(:circle_cell_at?))
+  end
+
+  def any_diagonal_only_circle?
+    any_diagonal_only?(@board.method(:circle_cell_at?))
   end
 end
--- tic-tac-toe/spec/board_analyzer_spec.rb
+++ tic-tac-toe/spec/board_analyzer_spec.rb
@@ -11,13 +11,184 @@ describe 'Board analyzer' do
     analyzer = BoardAnalyzer.new(board)
     expect(analyzer.game_finished?).to eq false
   end
+end
 
-  it 'should return true if the first row is filled with xs' do
+describe 'Game finished if 3 cross in a row' do
+  let(:board) { Board.new }
+  it 'should return true if the first row is filled with Xs' do
     player_one = PlayerOne.new
     board.make_move(Move.new(0, 0, player_one))
     board.make_move(Move.new(0, 1, player_one))
     board.make_move(Move.new(0, 2, player_one))
     analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second row is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(1, 0, player_one))
+    board.make_move(Move.new(1, 1, player_one))
+    board.make_move(Move.new(1, 2, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+
+  it 'should return true if the second row is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(2, 0, player_one))
+    board.make_move(Move.new(2, 1, player_one))
+    board.make_move(Move.new(2, 2, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+describe 'Game finished if 3 cross in a column' do
+  let(:board) { Board.new }
+  it 'should return true if the first column is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(0, 0, player_one))
+    board.make_move(Move.new(1, 0, player_one))
+    board.make_move(Move.new(2, 0, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second column is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(0, 1, player_one))
+    board.make_move(Move.new(1, 1, player_one))
+    board.make_move(Move.new(2, 1, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+
+  it 'should return true if the second column is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(0, 2, player_one))
+    board.make_move(Move.new(1, 2, player_one))
+    board.make_move(Move.new(2, 2, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+describe 'Game finished if 3 cross in a diagonal' do
+  let(:board) { Board.new }
+  it 'should return true if the first diagonal is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(0, 0, player_one))
+    board.make_move(Move.new(1, 1, player_one))
+    board.make_move(Move.new(2, 2, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second diagonal is filled with Xs' do
+    player_one = PlayerOne.new
+    board.make_move(Move.new(0, 2, player_one))
+    board.make_move(Move.new(1, 1, player_one))
+    board.make_move(Move.new(2, 0, player_one))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+# CIRCLES
+describe 'Game finished if 3 circles in a row' do
+  let(:board) { Board.new }
+  it 'should return true if the first row is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 0, player_two))
+    board.make_move(Move.new(0, 1, player_two))
+    board.make_move(Move.new(0, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second row is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(1, 0, player_two))
+    board.make_move(Move.new(1, 1, player_two))
+    board.make_move(Move.new(1, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+
+  it 'should return true if the second row is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(2, 0, player_two))
+    board.make_move(Move.new(2, 1, player_two))
+    board.make_move(Move.new(2, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+describe 'Game finished if 3 circles in a column' do
+  let(:board) { Board.new }
+  it 'should return true if the first column is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 0, player_two))
+    board.make_move(Move.new(1, 0, player_two))
+    board.make_move(Move.new(2, 0, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second column is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 1, player_two))
+    board.make_move(Move.new(1, 1, player_two))
+    board.make_move(Move.new(2, 1, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+
+  it 'should return true if the second column is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 2, player_two))
+    board.make_move(Move.new(1, 2, player_two))
+    board.make_move(Move.new(2, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+describe 'Game finished if 3 circles in a diagonal' do
+  let(:board) { Board.new }
+  it 'should return true if the first diagonal is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 0, player_two))
+    board.make_move(Move.new(1, 1, player_two))
+    board.make_move(Move.new(2, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+  it 'should return true if the second diagonal is filled with Os' do
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 2, player_two))
+    board.make_move(Move.new(1, 1, player_two))
+    board.make_move(Move.new(2, 0, player_two))
+    analyzer = BoardAnalyzer.new(board)
+    expect(analyzer.game_finished?).to eq true
+  end
+end
+
+describe 'Game finished by draw' do
+  let(:board) { Board.new }
+  it 'should return true if there are no winners no one has won' do
+    player_one = PlayerOne.new
+    player_two = PlayerTwo.new
+    board.make_move(Move.new(0, 0, player_one))
+    board.make_move(Move.new(1, 1, player_one))
+    board.make_move(Move.new(1, 2, player_one))
+    board.make_move(Move.new(2, 1, player_one))
+
+    board.make_move(Move.new(0, 1, player_two))
+    board.make_move(Move.new(0, 2, player_two))
+    board.make_move(Move.new(1, 0, player_two))
+    board.make_move(Move.new(2, 0, player_two))
+    board.make_move(Move.new(2, 2, player_two))
+    analyzer = BoardAnalyzer.new(board)
+
+    expect(analyzer.player_one_won?).to eq false
+    expect(analyzer.player_two_won?).to eq false
     expect(analyzer.game_finished?).to eq true
   end
 end
